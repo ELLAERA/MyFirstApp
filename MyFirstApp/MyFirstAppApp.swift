ae8eeac7fd754b7d5620dabe83cf31ea0f726e69
ELLAERA <qwer2407@gmail.com>
2023-04-06 16:04:20 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by 이애라 on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
