0e9fa1c8fb49dcc441e8a01f62a5e41c8986bd3c
ELLAERA <qwer2407@gmail.com>
2023-04-20 13:44:44 +0900

    Text

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -2,24 +2,41 @@
 //  ContentView.swift
 //  MyFirstApp
 //
-//  Created by 이애라 on 2023/04/06.
+//  Created by 이애라 on 2023/04/20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    let name: String = "ELLA"
+    var age: Int = 20
+    var hasJob: Bool = true
+    var animals1: Set = ["🐶", "🐱"]
+    var animals2: Set = ["🐶", "🐭", "🐭", "🐹"]
+    
     var body: some View {
-//        Button {
-//            print("Hited!!!")
-//        } label: {
-//            Text("ELLA")
-//                .padding()
-//                .frame(width: 150)
-//                .background(.yellow)
-//                .cornerRadius(15)
-//        }
-        Button("Delete", role: .destructive) {
-            print("deleted!")
+        VStack {
+            Text("HI")
+                .italic()
+                .underline(true, color: .mint)
+                .font(.system(size: 60, weight: .thin, design: .none))
+                .foregroundColor(.green)
+                .padding(30)
+            Text  (animals1.intersection(animals2).description)
+            
+            Button {
+                print("Hited!!!")
+            } label: {
+                Text("Hello, \(name) (\(age)) \n\(hasJob.description)")
+                    .padding()
+                    .frame(width: 150)
+                    .background(.yellow)
+                    .cornerRadius(15)
+            }
+            Button("Delete", role: .destructive) {
+                print("deleted!")
+            }
         }
     }
 }
